6ac77b92509f82477a16460f5611f3a2d88faa9b
kimyoungyin <yin199859@gmail.com>
2025-10-20 00:00:53 +0900

    feat: 2504 괄호의 값 첫 풀이

--- 2504-스택의활용.js
+++ 2504-스택의활용.js
@@ -5,10 +5,17 @@ let score = 0,
     isValid = true;
 
 const stack = [];
-const scoreStack = [];
+let smCount = 0,
+    lgCount = 0,
+    isValue = false;
 
 for (let char of input) {
-    if (char === "(" || char === "[") stack.push(char);
+    if (char === "(" || char === "[") {
+        if (char === "(") smCount++;
+        else lgCount++;
+        stack.push(char);
+        isValue = true; // 값의 시작점
+    }
     if (char === ")" || char === "]") {
         if (stack.length === 0) {
             isValid = false;
@@ -19,12 +26,32 @@ for (let char of input) {
                 isValid = false;
                 break;
             }
+            if (isValue) {
+                // console.log(
+                //     "소괄호 닫힘 발견, +",
+                //     Math.pow(2, smCount) * Math.pow(3, lgCount)
+                // );
+                score += Math.pow(2, smCount) * Math.pow(3, lgCount);
+            }
+            stack.pop();
+            smCount--;
         } else if (char === "]") {
             if (stack.at(-1) !== "[") {
                 isValid = false;
                 break;
             }
+            if (isValue) {
+                // console.log(
+                //     "대괄호 닫힘 발견, +",
+                //     Math.pow(2, smCount) * Math.pow(3, lgCount)
+                // );
+
+                score += Math.pow(2, smCount) * Math.pow(3, lgCount);
+            }
+            stack.pop();
+            lgCount--;
         }
+        isValue = false; // 이미 안에서 계산을 완료했기에 겉에 감싸진 스택 괄호는 계산에 추가하지 않음
     }
 }
 
