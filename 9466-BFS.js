28ca07c778af51d14a40eff517e7839254c7f766
kimyoungyin <yin199859@gmail.com>
2025-10-27 01:49:15 +0900

    feat: 9466 BFS로 상태를 구분하여 풀이해아 하는 어려운 문제

--- 9466-BFS.js
+++ 9466-BFS.js
@@ -1,44 +1,64 @@
+// 한 번 더 회전하는 것에 부담을 느끼지 말라 2*N = N
 const fs = require("fs");
 const lines = fs.readFileSync("dev/stdin").toString().trim().split("\n");
 
 const ans = [];
 
+const type = {
+    notVisited: 0,
+    visited: 1,
+    notCycle: 2,
+    cycle: 3,
+};
+
 for (let i = 1; i < lines.length; i += 2) {
-    const board = lines[i + 1].split(" ").map((order) => Number(order) - 1);
+    const board = lines[i + 1].split(" ").map((order) => Number(order) - 1); // 인덱스화
     // 사이클 도는 중
-    const vis = Array(board.length).fill(false);
+    const states = Array(board.length)
+        .fill()
+        .map(() => type.notVisited);
 
-    // bfs
-    for (let x = 0; x < board.length; x++) {
-        if (vis[x]) continue; // 이미 팀이 맺어졌으면 생략
-        const q = [];
-        let head = 0;
-        q.push([x, board[x], [x]]); // 현재 위치, 다음 지점, 팀짜기 시작 위치
-        vis[x] = true;
+    for (let x = 0; x < states.length; x++) {
+        if (states[x] !== type.notVisited) continue; // 방문한 적이 없으면 시작
+        // 초기화 및 bfs 진행
+        let curX = x;
+        while (true) {
+            states[curX] = type.visited;
+            curX = board[curX];
 
-        while (head < q.length) {
-            const [curX, nx, teams] = q[head++];
-
-            // nx === teams[0]면 nx도 추가하고 팀 확정 break;
-            if (nx === teams[0]) {
-                vis[nx] = true;
+            // 이미 사이클이 아니거나, 사이클임이 확정되었을 떄 -> 지금까지 방문했던 것은 모두 사이클이 아님
+            if (states[curX] === type.notCycle || states[curX] === type.cycle) {
+                curX = x;
+                while (states[curX] === type.visited) {
+                    states[curX] = type.notCycle;
+                    curX = board[curX];
+                }
                 break;
             }
-            // 아니면
-            if (!vis[nx]) {
-                // 1. nx 방문한 적이 없음 -> 이동
-                q.push([nx, board[nx], [...teams, nx]]);
-                vis[nx] = true;
-            } else {
-                // 2. nx 방문한 적이 있음 -> 팀이 이루어지지 않음 초기화 및 break;
-                for (let team of teams) {
-                    vis[team] = false;
+            // 방문했던 사람 y을 또 마주했을 때. x -> y 전까지는 사이클이 아니고, y -> y전까지는 사이클임
+            if (states[curX] === type.visited && curX !== x) {
+                while (states[curX] !== type.cycle) {
+                    states[curX] = type.cycle;
+                    curX = board[curX];
+                }
+                curX = x;
+                while (states[curX] !== type.cycle) {
+                    states[curX] = type.notCycle;
+                    curX = board[curX];
+                }
+                break;
+            }
+            // 자기 자신을 다시 마주쳤을 때: 지금까지 방문했던 것은 모두 사이클임
+            if (states[curX] === type.visited && curX === x) {
+                while (states[curX] !== type.cycle) {
+                    states[curX] = type.cycle;
+                    curX = board[curX];
                 }
                 break;
             }
         }
     }
-    ans.push(vis.filter((el) => !el).length);
+    ans.push(states.filter((state) => state === type.notCycle).length);
 }
 
 console.log(ans.join("\n"));
