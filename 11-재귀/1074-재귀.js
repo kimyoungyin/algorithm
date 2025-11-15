68760bb7597f9030192572e54e2eab50d9c6ebb4
kimyoungyin <yin199859@gmail.com>
2025-11-16 00:05:33 +0900

    feat: 1024 z 재귀 풀이

--- 11-재귀/1074-재귀.js
+++ 11-재귀/1074-재귀.js
@@ -1,3 +1,4 @@
+// r과 n은 0부터 카운트
 const fs = require("fs");
 const [N, r, c] = fs
     .readFileSync("../dev/stdin")
@@ -6,21 +7,18 @@ const [N, r, c] = fs
     .split(" ")
     .map(Number);
 
-// N = 1일 때 움직임은 정해져있다.
-// board[r][c]일 때 위치는 z(n - 1)
-
 function z(n, r, c) {
     if (n === 0) {
         return 0;
     }
     const val = Math.pow(2, n - 1);
-    if (r <= val && c <= val) {
+    if (r < val && c < val) {
         return z(n - 1, r, c);
     }
-    if (r <= val && c > val) {
+    if (r < val && c >= val) {
         return z(n - 1, r, c - val) + val * val;
     }
-    if (r > val && c <= val) {
+    if (r >= val && c < val) {
         return z(n - 1, r - val, c) + val * val * 2;
     }
     return z(n - 1, r - val, c - val) + val * val * 3;
