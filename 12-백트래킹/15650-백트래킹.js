e59c72ab1c7597fdbd763916329da5ac1dbf4dc2
kimyoungyin <yin199859@gmail.com>
2025-11-19 14:52:24 +0900

    feat: 15650 백트래킹 isUsed 사용하지 않는 풀이

--- 12-백트래킹/15650-백트래킹.js
+++ 12-백트래킹/15650-백트래킹.js
@@ -6,8 +6,7 @@ const [N, M] = fs
     .split(" ")
     .map(Number);
 
-const isUsed = Array(N + 1).fill(0);
-const arr = [];
+const arr = Array(M).fill(0);
 
 function solution(k) {
     if (k === M) {
@@ -15,13 +14,9 @@ function solution(k) {
         return;
     }
     // 맨 마지막 선택된 수 + 1부터 끝까지 중 탐색
-    for (let i = (arr[arr.length - 1] || 0) + 1; i <= N; i++) {
-        if (isUsed[i]) continue;
-        isUsed[i] = 1;
-        arr.push(i);
+    for (let i = (arr[k - 1] || 0) + 1; i <= N; i++) {
+        arr[k] = i;
         solution(k + 1);
-        isUsed[i] = 0;
-        arr.pop(i);
     }
 }
 
